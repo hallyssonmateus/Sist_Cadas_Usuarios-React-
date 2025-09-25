87d5ee6cc921c53606ae776127a1914b0952fa43
Hallysson Mateus <hallysson_gato@hotmail.com>
2025-09-25 01:43:12 -0300

    feat: Create 'Add Client' button

--- src/components/CardClients.tsx
+++ src/components/CardClients.tsx
@@ -4,9 +4,9 @@ import { GoTrash } from "react-icons/go";
 
 export default function CardClients() {
  return (
-    <div className="card bg-white w-[285px] h-[138px] p-2 rounded-xs shadow-sm flex flex-col justify- items-center gap-1">
+    <div className="card bg-white w-[245px] h-[138px] p-2 rounded-xs shadow-sm flex flex-col justify- items-center gap-1">
         <h2 className="font-bold">Eduardo</h2>
-        <p>Salario: R$3.500,00</p>
+        <p>Salário: R$3.500,00</p>
         <p>Empresa: R$120.000,00</p>
         <div className="flex justify-between p-2 w-full">
             <button className="text-lg cursor-pointer">
--- src/pages/ClientsList.tsx
+++ src/pages/ClientsList.tsx
@@ -6,14 +6,14 @@ export default function ClientsList() {
  return (
    <div className="bg-stone-200">
     <Navbar />
-    <section className="p-10">
+    <section className="p-10 flex flex-col max-w-6xl mx-auto">
         {/* Header content */}
         <div className="flex justify-between">
             <h2>16 clientes encontrados: </h2>
             <h2>Clientes por página: </h2>
         </div>
         {/* Clients List Cards*/}
-        <div className="flex flex-wrap gap-4 mt-2">
+        <div className="w-full flex flex-wrap gap-4 mt-2">
             <CardClients />
             <CardClients />
             <CardClients />
@@ -31,6 +31,9 @@ export default function ClientsList() {
             <CardClients />
             <CardClients />
         </div>
+        <button className="mt-4 p-2 text-sm font-bold cursor-pointer text-orange-600 text-bold w-full border-2 border border-orange-500 rounded-sm ">
+            Criar cliente
+        </button>
     </section>
    </div>
  );
