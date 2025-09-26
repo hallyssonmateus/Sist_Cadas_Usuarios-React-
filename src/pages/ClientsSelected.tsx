6d34b872b2f5aeaf9076f5c42daaf80a6496f613
Hallysson Mateus <hallysson_gato@hotmail.com>
2025-09-25 22:33:27 -0300

    feat: Implement ClientsSelected page and 'Clear' button

--- src/pages/ClientsSelected.tsx
+++ src/pages/ClientsSelected.tsx
@@ -1,10 +1,29 @@
+import CardClients from "../components/CardClients";
 import Navbar from "../components/Navbar";
 
 export default function ClientsSelected() {
  return (
-   <div>
+   <div className="bg-stone-200 h-screen">
     <Navbar />
-    <h1>Clients Selected Page</h1>
+    <section className="p-10 flex flex-col max-w-6xl mx-auto">
+      {/* Header content */}
+      <h1 className="font-bold">Clientes Selecionados:</h1>
+      {/* Selected Clients List Cards*/}
+      <div className="w-full flex flex-wrap gap-4 mt-2">
+        <CardClients />
+        <CardClients />
+        <CardClients />
+        <CardClients />
+        <CardClients />
+        <CardClients />
+        <CardClients />
+        <CardClients />
+      </div>
+      {/* Button Inferior */}
+      <button className="mt-4 p-2 text-sm font-bold cursor-pointer text-orange-600 text-bold w-full border-2 border border-orange-500 rounded-sm ">
+        Limpar clientes selecionados
+      </button>
+    </section>
    </div>
  );
 }
